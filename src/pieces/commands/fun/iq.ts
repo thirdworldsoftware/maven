5fd5b84ba3d70d284a009747fc53b66379768d5c
synesta <synestamail2@gmail.com>
2021-11-23 23:20:26 -0500

    feat(command): add IQ command

---
+++ src/pieces/commands/fun/iq.ts
@@ -0,0 +1,20 @@
+import { send } from '@sapphire/plugin-editable-commands';
+import { ApplyOptions } from '@sapphire/decorators';
+import { /* Args, */ Command, CommandOptions } from '@sapphire/framework';
+import { Message, MessageEmbed } from 'discord.js';
+
+@ApplyOptions<CommandOptions>({
+    name: 'iq',
+})
+
+export class InfoCommand extends Command {
+    public async messageRun(message: Message /*, args: Args */) {
+
+        const embed = new MessageEmbed()
+            .setDescription(`${message.author}'s IQ is ${Math.floor(Math.random() * 300) + 1}`)  /* I have no idea how to do this better */
+            .setColor('#5464af')
+
+        // Using the send function from the editable-commands plugin, not the base one
+        send(message, { embeds: [embed] });
+    }
+}
