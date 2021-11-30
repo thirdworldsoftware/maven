cae0775d2c37b5652a1d8916e44be4fad1a7040c
Legacy <legacystorm22@gmail.com>
2021-11-30 16:45:40 +0600

    feat(commands/econ): remove, reset, set subcommands for econ-admin

--- src/pieces/commands/economy/econ-admin.ts
+++ src/pieces/commands/economy/econ-admin.ts
@@ -20,6 +20,7 @@ const nBank: readonly kBank[] = ['bank', 'b'];
         'remove',
         'list',
         'reset',
+        'set',
 
         { input: 'rm', output: 'remove' },
         { input: 'ls', output: 'list' },
@@ -61,7 +62,112 @@ export class EconAdminCommand extends SubCommandPluginCommand {
             .setTitle(`Added ${amount} to ${user.tag}'s ${storageType}'`)
             .setColor('GREEN');
 
-        send(message, { embeds: [embed] });
+        return send(message, { embeds: [embed] });
+    }
+
+    public async remove(message: Message, args: Args) {
+        const user = await args.pick('user').catch(() => message.author);
+        const storageType = await args
+            .pick(EconAdminCommand.walletOrBank)
+            .catch(() => 'wallet');
+        const amount = await args.pick('number');
+
+        try {
+            const data =
+                storageType === 'wallet'
+                    ? { wallet: { decrement: amount } }
+                    : { bank: { decrement: amount } };
+
+            await prisma.user.update({
+                where: {
+                    discord_id: user.id,
+                },
+                data,
+            });
+        } catch (err) {
+            if (err instanceof Error) {
+                const embed = new MessageEmbed()
+                    .setTitle('Unknown error during operation')
+                    .setDescription(err.message)
+                    .setColor('RED');
+
+                return send(message, { embeds: [embed] });
+            }
+        }
+
+        const embed = new MessageEmbed()
+            .setTitle(`Removed ${amount} from ${user.tag}'s ${storageType}'`)
+            .setColor('GREEN');
+
+        return send(message, { embeds: [embed] });
+    }
+
+    public async reset(message: Message, args: Args) {
+        const user = await args.pick('user').catch(() => message.author);
+
+        try {
+            await prisma.user.update({
+                where: {
+                    discord_id: user.id,
+                },
+                data: {
+                    bank: 0,
+                    wallet: 0,
+                },
+            });
+        } catch (err) {
+            if (err instanceof Error) {
+                const embed = new MessageEmbed()
+                    .setTitle('Unknown error during operation')
+                    .setDescription(err.message)
+                    .setColor('RED');
+
+                return send(message, { embeds: [embed] });
+            }
+        }
+
+        const embed = new MessageEmbed()
+            .setTitle(`Reset ${user.tag}`)
+            .setColor('GREEN');
+
+        return send(message, { embeds: [embed] });
+    }
+
+    public async set(message: Message, args: Args) {
+        const user = await args.pick('user').catch(() => message.author);
+        const storageType = await args
+            .pick(EconAdminCommand.walletOrBank)
+            .catch(() => 'wallet');
+        const amount = await args.pick('number');
+
+        try {
+            const data =
+                storageType === 'wallet'
+                    ? { wallet: amount }
+                    : { bank: amount };
+
+            await prisma.user.update({
+                where: {
+                    discord_id: user.id,
+                },
+                data,
+            });
+        } catch (err) {
+            if (err instanceof Error) {
+                const embed = new MessageEmbed()
+                    .setTitle('Unknown error during operation')
+                    .setDescription(err.message)
+                    .setColor('RED');
+
+                return send(message, { embeds: [embed] });
+            }
+        }
+
+        const embed = new MessageEmbed()
+            .setTitle(`Added ${amount} to ${user.tag}'s ${storageType}'`)
+            .setColor('GREEN');
+
+        return send(message, { embeds: [embed] });
     }
 
     private static walletOrBank = Args.make<'bank' | 'wallet'>(
